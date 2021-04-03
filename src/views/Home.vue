bbd168733652b85f6e2c352815ad15b6129de9de
Reidond <reidond@gmail.com>
2021-04-03 13:06:17 +0300

    feat: add calc view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,67 +2,244 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>EEI</ion-title>
       </ion-toolbar>
     </ion-header>
-    
+
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
+          <ion-title size="large">EEI</ion-title>
         </ion-toolbar>
       </ion-header>
-    
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+
+      <div class="container">
+        <ion-textarea
+          class="textarea"
+          readonly
+          v-model="textareaRef"
+        ></ion-textarea>
+        <div class="actions-container">
+          <ion-row v-for="(r, i) in controlsRef" :key="`${r}${i}`">
+            <ion-col v-for="(c, ci) in r" :key="`${c}${ci}`">
+              <ion-button
+                @click="onControlPress(c)"
+                expand="block"
+                size="large"
+                color="light"
+                >{{ c }}</ion-button
+              >
+            </ion-col>
+          </ion-row>
+        </div>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
-<script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
+<script>
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonTextarea,
+  IonRow,
+  IonCol,
+  IonButton,
+} from "@ionic/vue";
+import { defineComponent, ref } from "vue";
+import { useSwissCalcCalculator } from "@/use/swisscalc_calculator.js";
+import {
+  useDivisionOperator,
+  useMultiplicationOperator,
+  useSubtractionOperator,
+  useAdditionOperator,
+  usePercentOperator,
+} from "@/use/swisscalc_operator_cache.js";
 
 export default defineComponent({
-  name: 'Home',
+  name: "Home",
   components: {
     IonContent,
     IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar
-  }
+    IonToolbar,
+    IonTextarea,
+    IonRow,
+    IonCol,
+    IonButton,
+  },
+  setup() {
+    const textareaRef = ref("0");
+    const controlsRef = ref([
+      ["AC", "+/-", "%", "/"],
+      ["7", "8", "9", "x"],
+      ["4", "5", "6", "-"],
+      ["1", "2", "3", "+"],
+      ["0", ",", "="],
+    ]);
+
+    const {
+      addDigit,
+      addBinaryOperator,
+      addUnaryOperator,
+      negate,
+      clear,
+      equalsPressed,
+      getMainDisplay,
+    } = useSwissCalcCalculator();
+
+    const onControlPress = (control) => {
+      const actions = {
+        "0": {
+          type: "digit",
+          action: () => {
+            addDigit(control);
+          },
+        },
+        "1": {
+          type: "digit",
+          action: () => {
+            addDigit(control);
+          },
+        },
+        "2": {
+          type: "digit",
+          action: () => {
+            addDigit(control);
+          },
+        },
+        "3": {
+          type: "digit",
+          action: () => {
+            addDigit(control);
+          },
+        },
+        "4": {
+          type: "digit",
+          action: () => {
+            addDigit(control);
+          },
+        },
+        "5": {
+          type: "digit",
+          action: () => {
+            addDigit(control);
+          },
+        },
+        "6": {
+          type: "digit",
+          action: () => {
+            addDigit(control);
+          },
+        },
+        "7": {
+          type: "digit",
+          action: () => {
+            addDigit(control);
+          },
+        },
+        "8": {
+          type: "digit",
+          action: () => {
+            addDigit(control);
+          },
+        },
+        "9": {
+          type: "digit",
+          action: () => {
+            addDigit(control);
+          },
+        },
+        "/": {
+          type: "operator",
+          op: useDivisionOperator(),
+          action: (op) => {
+            addBinaryOperator(op);
+          },
+        },
+        x: {
+          type: "operator",
+          op: useMultiplicationOperator(),
+          action: (op) => {
+            addBinaryOperator(op);
+          },
+        },
+        "-": {
+          type: "operator",
+          op: useSubtractionOperator(),
+          action: (op) => {
+            addBinaryOperator(op);
+          },
+        },
+        "+": {
+          type: "operator",
+          op: useAdditionOperator(),
+          action: (op) => {
+            addBinaryOperator(op);
+          },
+        },
+        "%": {
+          type: "operator",
+          op: usePercentOperator(),
+          action: (op) => {
+            addUnaryOperator(op);
+          },
+        },
+        "+/-": {
+          type: "common",
+          action: () => {
+            negate();
+          },
+        },
+        AC: {
+          type: "common",
+          action: () => {
+            clear();
+          },
+        },
+        "=": {
+          type: "common",
+          action: () => {
+            equalsPressed();
+          },
+        },
+      };
+      actions[control].type === "operator" &&
+        actions[control].action(actions[control].op);
+      (actions[control].type === "digit" ||
+        actions[control].type === "common") &&
+        actions[control].action();
+      const mainDisplayValue = getMainDisplay();
+      textareaRef.value = mainDisplayValue;
+    };
+
+    return {
+      textareaRef,
+      controlsRef,
+      onControlPress,
+    };
+  },
 });
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
+.textarea {
+  --padding-end: var(--padding-top);
+  text-align: end;
+  font-weight: normal;
+  font-size: 24px;
 }
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
+.container {
+  display: flex;
+  flex-flow: column;
+  min-height: calc(100vh - var(--offset-top));
 }
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
+.actions-container {
+  min-height: 370px;
+  margin-top: auto;
 }
 </style>
